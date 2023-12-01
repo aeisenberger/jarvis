e1733d68e7e6461e026c8efc0d62a0fd7a217c6d
Avi Eisenberger <avi@angellist.com>
2023-11-30 16:37:08 -0800

    change path

--- download.sh
+++ download.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 cd ~;
-git clone https://github.com/AngelList/jarvis.git;
+git clone https://github.com/aeisenberger/jarvis.git;
 git checkout -b $USER
 echo "Running Jarvis setup (setup.sh)"
 source setup.sh
